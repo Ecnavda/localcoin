d4e3fc98ea87d3c15d045f4148ee647aa2fb3662
Isabella Mattua <48728386+isabellamattua@users.noreply.github.com>
2021-09-25 01:40:30 -0400

    Basic Navigation/Created 3 Activities
    Basic Navigation and created 3 activities (MainMenu, Login and Portfolio).

--- app/src/main/java/com/example/localcoin/MainActivity.kt
+++ app/src/main/java/com/example/localcoin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.localcoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signupButton = findViewById<Button>(R.id.signupButton)
+
+        loginButton.setOnClickListener{
+            val intention = Intent(this, LoginActivity::class.java)
+            this.startActivity(intention)
+        }
     }
 }
