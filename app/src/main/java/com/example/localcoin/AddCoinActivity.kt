d5e462897522fd46b943d003dbccddcc7bfb30a6
Ecnavda <ecnavda@gmail.com>
2021-09-25 23:48:36 -0400

    Added spinner.

--- app/src/main/java/com/example/localcoin/AddCoinActivity.kt
+++ app/src/main/java/com/example/localcoin/AddCoinActivity.kt
@@ -6,9 +6,7 @@ import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import java.io.*
 import java.lang.StringBuilder
 
@@ -18,14 +16,23 @@ class AddCoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_coin)
 
-        val coinNameTextView = findViewById<TextView>(R.id.coinName)
+        //val coinNameTextView = findViewById<TextView>(R.id.coinName)
         val numbCointextView = findViewById<TextView>(R.id.numberOfCoin)
         val getCoinButton = findViewById<Button>(R.id.getCoinB)
 
-
+        val coinSpin = findViewById<Spinner>(R.id.coinNameSpin)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.coin_spinner,
+            R.layout.spinner_style
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            coinSpin.adapter = adapter
+        }
 
         getCoinButton.setOnClickListener {
-            val coinName = coinNameTextView.text.toString()
+            //val coinName = coinNameTextView.text.toString()
+            val coinName = coinSpin.selectedItem.toString()
             val numberCoin = numbCointextView.text.toString()
             println(coinName)
             println("I am dumb")
